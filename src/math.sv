8cf956d83163658d41c31641fd9c2e71fc7b8a9a
Sophia Li <72220938+sophialiCMU@users.noreply.github.com>
2022-12-01 17:17:51 -0500

    update input / output names

--- src/math.sv
+++ src/math.sv
@@ -11,25 +11,36 @@
 // 2 7-segment displays
 
 module sophialiCMU_math (
-  input  logic       clock, reset, en,
-  input  logic [2:0] in,
-  input  logic [1:0] arithOp,
-  output logic [7:0] out
+  input logic io_in,
+  output logic io_out  
 );
+  
+  logic clock, reset, en;
+  logic [2:0] in;
+  logic [1:0] arithOp;
+  
+  always_comb begin
+    clock = io_in[0];
+    reset = io_in[1];
+    en = io_in[2];
+    in = io_in[3:5];
+    arithOp = io_n[6:7];
+  end
+
   logic enable;
   enum logic {IDLE, GO} state, nextState;
 
   // ALU
   always_ff @(posedge clock, posedge reset) begin
     if (reset)
-      out <= 0;
+      io_out <= 0;
     else begin
       if (enable)
         unique case (arithOp)
-          2'b00: out <= out + in;  // add
-          2'b01: out <= out - in;  // subtract
-          2'b10: out <= out ^ {5'b0, in};  // XOR per bit
-          2'b11: out <= out << in;  // Left-shift per bit
+          2'b00: io_out <= io_out + in;  // add
+          2'b01: io_out <= io_out - in;  // subtract
+          2'b10: io_out <= io_out ^ {5'b0, in};  // XOR per bit
+          2'b11: io_out <= io_out << in;  // Left-shift per bit
         endcase
     end
   end
